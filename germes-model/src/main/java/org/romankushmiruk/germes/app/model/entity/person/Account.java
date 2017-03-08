71a8783c53337765effb999f4f6d4cba823b6d0d
Roman <rKushmiruk@gmail.com>
2017-03-08 21:03:40 +0200

    defining our entities: settlement and station

---
+++ germes-model/src/main/java/org/romankushmiruk/germes/app/model/entity/person/Account.java
@@ -0,0 +1,11 @@
+package org.romankushmiruk.germes.app.model.entity.person;
+
+import org.romankushmiruk.germes.app.model.entity.base.AbstractEntity;
+
+/**
+ * Entity that encapsulates user of the application
+ * @author RomanKushmiruk
+ *
+ */
+public class Account extends AbstractEntity {
+}
---
+++ germes-model/src/main/java/org/romankushmiruk/germes/app/model/entity/transport/TransportType.java
@@ -0,0 +1,11 @@
+package org.romankushmiruk.germes.app.model.entity.transport;
+
+ /**
+ * Type of the station/transport/vehicle
+ * that we support in the application
+ * @author RomanKushmiruk
+ *
+ */
+public enum TransportType {
+    AUTO, RAILWAY, AVIA;
+}
